be6abe1be14783d6b9ff41969f3ac4ce8874bff7
Glen_Pan <66463241+QROW202011@users.noreply.github.com>
2021-10-17 23:13:02 +0800

    Add files via upload

---
+++ a_greater_than_b.py
@@ -0,0 +1,10 @@
+#yijie pan
+#a_greater than_b
+#this task is a comparision
+a=input("press an integer")
+b=input('press an integer')
+c=bool(int(a)>int(b))  #it will understand whether it has a int or not.
+print(c)
+
+
+
---
+++ a_greater_than_b_2.py
@@ -0,0 +1,14 @@
+#yijie pan
+#a_greater than_b
+#this task is a comparision
+a=input("press an integer")
+b=input('press an integer')
+if (a>b)
+    print("the integer", a,"is bigger than ",b)
+if a==b:
+    print('the integer ',a,' equals to ',b)
+else:
+    print('the integer ',b,' is bigger than',a)
+
+
+
